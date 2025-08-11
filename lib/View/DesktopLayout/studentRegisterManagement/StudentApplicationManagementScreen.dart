da8245fec75ffdf06dd53b9a2ad804ad0b2d7838
Sye0x <s.r.mahamid@gmail.com>
2025-08-11 20:56:48 +0500

    Creating Student Admission Management Screen

---
+++ lib/View/DesktopLayout/studentRegisterManagement/StudentApplicationManagementScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Studentapplicationmanagementscreen extends StatelessWidget {
+  const Studentapplicationmanagementscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: (Column(
+            children: [Text("Student Application Management Screen")],
+          )),
+        ),
+      ),
+    );
+  }
+}
