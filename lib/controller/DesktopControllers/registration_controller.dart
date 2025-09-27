51901b2ac6a47a6d67cd4840839da17deb18ef5d
Sye0x <s.r.mahamid@gmail.com>
2025-09-27 17:03:11 +0500

    User Registering correctly now through firebase

---
+++ lib/controller/DesktopControllers/registration_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegistrationController {
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final BuildContext context;
+
+  RegistrationController({
+    required this.emailController,
+    required this.passwordController,
+    required this.context,
+  });
+
+  Future<void> registerUser() async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: emailController.text.trim(),
+            password: passwordController.text.trim(),
+          );
+
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Registered: ${credential.user?.email}")),
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = "An error occurred";
+      if (e.code == 'weak-password') {
+        message = 'Password is too weak';
+      } else if (e.code == 'email-already-in-use') {
+        message = 'Email already registered';
+      }
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(message)));
+    }
+  }
+}
